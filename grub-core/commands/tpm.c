548b9387509ea076fc2358dba7bae2e5843179a4
Lu Ken <ken.lu@intel.com>
2021-09-28 00:05:15 +0800

    Enable TDX measurement to RTMR register
    Intel Trust Domain Extensions(Intel TDX) refers to an Intel technology
    that extends Virtual Machine Extensions(VMX) and Multi-Key Total Memory
    Encryption(MK-TME) with a new kind of virtual machine guest called a
    Trust Domain(TD)[1]. A TD runs in a CPU mode that protects the confidentiality
    of its memory contents and its CPU state from any other software, including
    the hosting Virtual Machine Monitor (VMM).
    
    Trust Domain Virtual Firmware (TDVF) is required to provide TD services to
    the TD guest OS.[2] Its reference code is available at https://github.com/tianocore/edk2-staging/tree/TDVF.
    
    To support TD measurement/attestation, TDs provide 4 RTMR registers like
    TPM/TPM2 PCR as below:
    - RTMR[0] is for TDVF configuration
    - RTMR[1] is for the TD OS loader and kernel
    - RTMR[2] is for the OS application
    - RTMR[3] is reserved for special usage only
    
    This patch adds TD Measurement protocol support along with TPM/TPM2 protocol.
    
    References:
    [1] https://software.intel.com/content/dam/develop/external/us/en/documents/tdx-whitepaper-v4.pdf
    [2] https://software.intel.com/content/dam/develop/external/us/en/documents/tdx-virtual-firmware-design-guide-rev-1.pdf
    
    Signed-off-by: Lu Ken <ken.lu@intel.com>

--- grub-core/commands/tpm.c
+++ grub-core/commands/tpm.c
@@ -22,6 +22,7 @@
 #include <grub/i18n.h>
 #include <grub/misc.h>
 #include <grub/mm.h>
+#include <grub/tdx.h>
 #include <grub/tpm.h>
 #include <grub/term.h>
 #include <grub/verify.h>
@@ -33,6 +34,7 @@ grub_err_t
 grub_tpm_measure (unsigned char *buf, grub_size_t size, grub_uint8_t pcr,
 		  const char *description)
 {
+  grub_tdx_log_event(buf, size, pcr, description);
   return grub_tpm_log_event (buf, size, pcr, description);
 }
 
